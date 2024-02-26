c7aacf1a5236976972596838b4eb71c841ab745f
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-02-26 17:07:24 +0530

    feat: added resource settings

--- src/app/instrument.ts
+++ src/app/instrument.ts
@@ -7,9 +7,18 @@ import {
 } from '@opentelemetry/sdk-trace-web';
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { Resource } from '@opentelemetry/resources';
 import { B3Propagator } from '@opentelemetry/propagator-b3';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 
-const provider = new WebTracerProvider();
+const resource = Resource.default().merge(
+  new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'signoz-angular-native',
+    [SemanticResourceAttributes.SERVICE_VERSION]: '0.1.0',
+  })
+);
+
+const provider = new WebTracerProvider({ resource });
 
 provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 
